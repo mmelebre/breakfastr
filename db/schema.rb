d5bd631b4c39f46adf8754b9b2650842fcf41518
Angelica da Rocha <angelicadarocha@angelica-da-rochas-macbook-10.local>
2015-03-05 17:07:18 +0000

    add user id to pastries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305152203) do
+ActiveRecord::Schema.define(version: 20150305170258) do
 
   create_table "pastries", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150305152203) do
     t.text     "description"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "user_id"
   end
+
+  add_index "pastries", ["user_id"], name: "index_pastries_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
