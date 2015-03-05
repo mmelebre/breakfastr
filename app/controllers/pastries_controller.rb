f6c21aae913245478a587462a1ffed85554b3f8f
Angelica da Rocha <angelicadarocha@angelica-da-rochas-macbook-10.local>
2015-03-05 15:23:35 +0000

    generate user model

--- app/controllers/pastries_controller.rb
+++ app/controllers/pastries_controller.rb
@@ -9,8 +9,46 @@ class PastriesController < ApplicationController
   end
 
   def new
+    @pastry = Pastry.new
+  end
+
+  def create
+    @pastry = Pastry.new(pastry_params)
+    if @pastry.save 
+        flash[:success] = "Pastry added!"
+        redirect_to root_path
+    else
+      flash[:error] = "Oops, something went wrong! Try again!"  
+      render :new
+    end     
   end
 
   def edit
+    @pastry = Pastry.find(params[:id])
+  end
+
+  def update
+    @pastry = Pastry.find(params[:id])
+    if @pastry.update(pastry_params)
+      flash[:success] = "Pastry successfully updated"
+      redirect_to pastry_path(@pastry)
+    else
+      flash[:error] = "Try again!"
+      render:edit  
+    end  
+  end
+
+  def destroy
+    @pastry = Pastry.find(params[:id])
+    @pastry.destroy
+    flash[:success] = "You destroyed a perfectly good pastry!!"
+    redirect_to root_path
+      
+  end  
+
+  private
+  def pastry_params
+    params.require(:pastry).permit(:name, :price_in_pence, :description)
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
   root "pastries#index"
-  resources :pastries
+  # The resources below essentially gives more URLs. The resources :pastries nested inside resources :users essentially makes the URL /users/:id/patries/new for everything except index and show
+  resources :pastries, only: [:index, :show]
+  resources :users do
+
+  	resources :pastries, except: [:index, :show]
+end
 
 end
