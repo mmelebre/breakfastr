061ed710d12b62cd797477bf79ad25a360cee5f0
Angelica da Rocha <angelicadarocha@angelica-da-rochas-macbook-10.local>
2015-03-05 12:40:20 +0000

    define pastries routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'pastries/index'
-
-  get 'pastries/show'
-
-  get 'pastries/new'
-
-  get 'pastries/edit'
-
   root "pastries#index"
+  resources :pastries
 
 end
